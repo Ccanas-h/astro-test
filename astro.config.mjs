e9f0561dd4f7e2dbed9cfe60c7ba83ddba0cc14f
ccanas-pc <ccanas.h@gmail.com>
2024-08-18 20:36:34 -0400

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import analogjsangular from "@analogjs/astro-angular";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [preact(), analogjsangular()]
+});
